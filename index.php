1837494ad4d68e63e8428420fb79ec76131cfa3c
Ayoun Paul Neel <86883078+neel123go@users.noreply.github.com>
2021-08-31 15:01:23 +0600

    Added best sales section & responsive it

--- index.php
+++ index.php
@@ -1,4 +1,188 @@
 <?php include 'inc/header.php'; ?>
 <?php include 'inc/slider.php'; ?>
 
+    <section class="best-sales">
+        <div class="bodycontainer">
+            <h2 class="heading">best sales</h2>
+            <div class="main">
+            <div class="card">
+                <div class="imgbox">
+                    <img src="assets/sliderimg-1.png" alt="">
+                    <ul class="action">
+                        <a href="#">
+                            <li>
+                                <i class="fa fa-heart" aria-hidden="true"></i>
+                                <span>Add to Wishlist</span>
+                            </li>
+                        </a>
+
+                        <a href="#">
+                            <li class="sec">
+                                <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                                <span>Add to Cart</span>
+                            </li>
+                        </a>
+
+                        <a href="#">
+                            <li class="thi">
+                                <i class="fa fa-eye" aria-hidden="true"></i>
+                                <span>View Details</span>
+                            </li>
+                        </a>
+                    </ul>
+                </div>
+
+                <div class="content">
+                    <div class="product-name">
+                        <h3>A single piace Neckless</h3>
+                    </div>
+                    <div class="price">
+                        <h2>৳ 3500</h2>
+                        <div class="rateing">
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star gray" aria-hidden="true"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="imgbox">
+                    <img src="assets/sliderimg-2.png" alt="">
+                    <ul class="action">
+                        <a href="#">
+                            <li>
+                                <i class="fa fa-heart" aria-hidden="true"></i>
+                                <span>Add to Wishlist</span>
+                            </li>
+                        </a>
+
+                        <a href="#">
+                            <li class="sec">
+                                <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                                <span>Add to Cart</span>
+                            </li>
+                        </a>
+
+                        <a href="#">
+                            <li class="thi">
+                                <i class="fa fa-eye" aria-hidden="true"></i>
+                                <span>View Details</span>
+                            </li>
+                        </a>
+                    </ul>
+                </div>
+
+                <div class="content">
+                    <div class="product-name">
+                        <h3>Gold plate banggle</h3>
+                    </div>
+                    <div class="price">
+                        <h2>৳ 3200</h2>
+                        <div class="rateing">
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="imgbox">
+                    <img src="assets/sliderimg-6.png" alt="">
+                    <ul class="action">
+                        <a href="#">
+                            <li>
+                                <i class="fa fa-heart" aria-hidden="true"></i>
+                                <span>Add to Wishlist</span>
+                            </li>
+                        </a>
+
+                        <a href="#">
+                            <li class="sec">
+                                <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                                <span>Add to Cart</span>
+                            </li>
+                        </a>
+
+                        <a href="#">
+                            <li class="thi">
+                                <i class="fa fa-eye" aria-hidden="true"></i>
+                                <span>View Details</span>
+                            </li>
+                        </a>
+                    </ul>
+                </div>
+
+                <div class="content">
+                    <div class="product-name">
+                        <h3>Wedding chokker</h3>
+                    </div>
+                    <div class="price">
+                        <h2>৳ 4000</h2>
+                        <div class="rateing">
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="imgbox">
+                    <img src="assets/sliderimg-4.png" alt="">
+                    <ul class="action">
+                        <a href="#">
+                            <li>
+                                <i class="fa fa-heart" aria-hidden="true"></i>
+                                <span>Add to Wishlist</span>
+                            </li>
+                        </a>
+
+                        <a href="#">
+                            <li class="sec">
+                                <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                                <span>Add to Cart</span>
+                            </li>
+                        </a>
+
+                        <a href="#">
+                            <li class="thi">
+                                <i class="fa fa-eye" aria-hidden="true"></i>
+                                <span>View Details</span>
+                            </li>
+                        </a>
+                    </ul>
+                </div>
+
+                <div class="content">
+                    <div class="product-name">
+                        <h3>Neckless</h3>
+                    </div>
+                    <div class="price">
+                        <h2>৳ 2000</h2>
+                        <div class="rateing">
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                            <i class="fa fa-star gray" aria-hidden="true"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            </div>
+        </div>
+    </section>
+
 <?php include 'inc/footer.php'; ?>
